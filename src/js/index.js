da35db7c766f09bfe7fdb8b05776d4bf071cbe29
no3utufff89 <14198908@mail.ru>
2025-04-05 00:59:58 +0300

    slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import { ceateBreads } from './modules/breadCrumbs.js';
 import { getPageElements } from './modules/getPageElements.js';
-
+import './module/swiper-bundle.min.js';
 const pageElements = getPageElements();
 pageElements.breadCrumbLinks.at(-1).classList.add('bread-crumbs__link_active');
 const elemsList = pageElements.breadCrumbElems.slice(1, pageElements.breadCrumbElems.length);
@@ -42,3 +42,31 @@ export const acc = () => {
     });
 };
 acc();
+new Swiper('.swiper', {
+    loop: true,
+    speed: 400,
+    slidesPerView: 2,
+    loopedSlides: 1,
+    grabCursor: true,
+    allowTouchMove: true,
+    navigation: {
+        nextEl: '.swiper-button_next',
+        prevEl: '.swiper-button_prev',
+    },
+
+    breakpoints: {
+        1440: {
+            allowTouchMove: true,
+            slidesPerView: 2,
+            loopedSlides: 3,
+        },
+        1024: {
+            slidesPerView: 3,
+            loopedSlides: 3,
+        },
+        768: {
+            slidesPerView: 2,
+            loopedSlides: 2,
+        },
+    },
+});
